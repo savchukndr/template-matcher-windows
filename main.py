fixed scale and rotation

main.py
@@ -19,14 +19,21 @@ class Main:
         # self.image_key = '2018/07/27-19:44:31_sava_1'
 
     @staticmethod
-    def rotate_image(img, side=90):
-        (h, w) = img.shape[:2]
-        center = (w / 2, h / 2)
-        M = cv2.getRotationMatrix2D(center, side, 1.0)
-        return cv2.warpAffine(img, M, (w, h))
+    def rotate_image(image, angle):
+        (h, w) = image.shape[:2]
+        (cX, cY) = (w // 2, h // 2)
+        M = cv2.getRotationMatrix2D((cX, cY), -angle, 1.0)
+        cos = np.abs(M[0, 0])
+        sin = np.abs(M[0, 1])
+        nW = int((h * sin) + (w * cos))
+        nH = int((h * cos) + (w * sin))
+        M[0, 2] += (nW / 2) - cX
+        M[1, 2] += (nH / 2) - cY
+        return cv2.warpAffine(image, M, (nW, nH))
 
     @staticmethod
     def downsize(img, img_tpl, width, height):
+        """Zoom in image"""
         print("start down")
         print("shape 1", img.shape[1])
         print("shape 0", img.shape[0])
@@ -37,14 +44,12 @@ class Main:
 
                 if x == 0:
                     img_res = cv2.resize(img, (width, height), interpolation=cv2.INTER_AREA)
-                    # img_res = cv2.resize(img, dim)
                 else:
                     img_res = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
-                    # img_res = cv2.resize(img, dim)
                     # cv2.imwrite(
-                    #     "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\QQQ{}.jpg".format(x), img_res)
-                # Match map building
+                    #     "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\QQQ_{}.jpg".format(x), img_res)
 
+                # Match map building
                 match_map = cv2.matchTemplate(img_res, img_tpl, cv2.TM_CCOEFF_NORMED)
             except Exception as e:
                 print("end down", e)
@@ -52,31 +57,28 @@ class Main:
 
             max_match_map = np.max(match_map)  # The value of the map for the region closest to the template
             print("x = {}, map = {}".format(x, max_match_map))
-            if max_match_map < 0.77:
+            if max_match_map < 0.71:
                 if x == 10000:
                     return []
             else:
-                return match_map, max_match_map
+                return match_map, max_match_map, img_res
 
     @staticmethod
     def upsize(img, img_tpl, width, height):
+        """Zoom out image"""
         print("start up")
-        for x in range(0, 10000, 100):
+        for x in range(0, 8700, 100):
             try:
                 r = (float(width) + x) / img.shape[1]
                 dim = (width + x, int(img.shape[0] * r))
 
                 if x == 0:
                     img_res = cv2.resize(img, (width, height), interpolation=cv2.INTER_CUBIC)
-                    # img_res = cv2.resize(img, (width, height))
                 else:
                     img_res = cv2.resize(img, dim, interpolation=cv2.INTER_CUBIC)
-                    # img_res = cv2.resize(img, dim)
-                    # cv2.imwrite(
-                    #     "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\LLL{}.jpg".format(x),
-                    #     img_res)
-                # Match map building
 
+                # cv2.imwrite("C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\LLL_{}.jpg".format(x), img_res)
+                # Match map building
                 match_map = cv2.matchTemplate(img_res, img_tpl, cv2.TM_CCOEFF_NORMED)
             except Exception as e:
                 print("end down", e)
@@ -84,27 +86,23 @@ class Main:
 
             max_match_map = np.max(match_map)  # The value of the map for the region closest to the template
             print("x = {}, map = {}".format(x, max_match_map))
-            if max_match_map < 0.77:
-                if x == 10000:
+            if max_match_map < 0.71:
+                if x == 8700:
                     return []
             else:
-                return match_map, max_match_map
+                return match_map, max_match_map, img_res
 
     @staticmethod
     def find_templ(img, img_tpl):
-        # img_canny = cv2.Canny(img, 100, 200)
-        # img_tpl_canny = cv2.Canny(img_tpl, 200, 400)
-        #
-        # # cv2.imwrite(
-        # #     "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\res1.jpg", img_canny)
-        # cv2.imwrite(
-        #     "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\result\\res.jpg", img_tpl_canny)
-        #####################
         # Template shape
         global max_match_map
 
         f = False
+        res = []
+        tmp = img_tpl
+        image_to_draw = None
         for degree in [0, 90, 270]:
+            img_tpl = tmp
             img_tpl = main.rotate_image(img_tpl, degree)
             h, w = img_tpl.shape
             height, width = img.shape
@@ -113,50 +111,54 @@ class Main:
             if not tpl:
                 tpl = main.upsize(img, img_tpl, width, height)
                 if not tpl:
-                    f = False
+                    res.append(None)
                 else:
-                    f = True
                     match_map = tpl[0]
                     max_match_map = tpl[1]
-                    break
+                    image_to_draw = tpl[2]
+                    a = 0.7  # Coefficient of "similarity", 0 - all, 1 - exact match
+                    # Cut the map on the threshold
+                    match_map = (match_map >= max_match_map * a) * match_map
+                    # Select local max on the map
+                    match_map_max = maximum_filter(match_map, size=min(w, h))
+                    # Areas closest to the pattern
+                    match_map = np.where((match_map == match_map_max), match_map, 0)
+                    # Coordinates of local max
+                    ii = np.nonzero(match_map)
+                    rr = tuple(zip(*ii))
+                    res.append([[c[1], c[0], w, h] for c in rr])
             else:
                 f = True
                 match_map = tpl[0]
                 max_match_map = tpl[1]
-                break
+                image_to_draw = tpl[2]
+                a = 0.7  # Coefficient of "similarity", 0 - all, 1 - exact match
+                # Cut the map on the threshold
+                match_map = (match_map >= max_match_map * a) * match_map
+                # Select local max on the map
+                match_map_max = maximum_filter(match_map, size=min(w, h))
+                # Areas closest to the pattern
+                match_map = np.where((match_map == match_map_max), match_map, 0)
+                # Coordinates of local max
+                ii = np.nonzero(match_map)
+                rr = tuple(zip(*ii))
+                res.append([[c[1], c[0], w, h] for c in rr])
 
         if not f:
-            return []
-
-
-
-
-        a = 0.7  # Coefficient of "similarity", 0 - all, 1 - exact match
-
-        # Cut the map on the threshold
-        match_map = (match_map >= max_match_map * a) * match_map
-
-        # Select local max on the map
-        match_map_max = maximum_filter(match_map, size=min(w, h))
-        # Areas closest to the pattern
-        match_map = np.where((match_map == match_map_max), match_map, 0)
-
-        # Coordinates of local max
-        ii = np.nonzero(match_map)
-        rr = tuple(zip(*ii))
-
-        res = [[c[1], c[0], w, h] for c in rr]
-
-        return res
+            res.append(None)
+        return res, image_to_draw
 
     # Draw a frames of matches found
     @staticmethod
     def draw_frames(img, coord):
+        """Drawing frames on a result image"""
         res = img.copy()
-        for c in coord:
-            top_left = (c[0], c[1])
-            bottom_right = (c[0] + c[2], c[1] + c[3])
-            cv2.rectangle(res, top_left, bottom_right, color=(0, 0, 255), thickness=5)
+        for x in coord:
+            if x is not None:
+                for c in x:
+                    top_left = (c[0], c[1])
+                    bottom_right = (c[0] + c[2], c[1] + c[3])
+                    cv2.rectangle(res, top_left, bottom_right, color=(0, 0, 255), thickness=5)
         return res
 
     # Crop enter image into shelfs
@@ -188,29 +190,28 @@ class Main:
 
     def main(self):
         # Connect to redis and get image to proceed
-        # postgres = PostgreSQL()
-        # agreement_id = postgres.select_agreement_id(self.image_key)
-        # product_title = postgres.select_product_title(str(agreement_id[0]))
-        # product_type_title = postgres.select_product_type_tytle(product_title[0])
+        postgres = PostgreSQL()
+        agreement_id = postgres.select_agreement_id(self.image_key)
+        product_title = postgres.select_product_title(str(agreement_id[0]))
+        product_type_title = postgres.select_product_type_tytle(product_title[0])
         global match_count, shelf
-        # redis = Redis(self.image_key)
-        # redis.get_image()
+        redis = Redis(self.image_key)
+        redis.get_image()
 
         # TODO: change to image
-        enter_image_path = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\image\\image2.jpg"
+        enter_image_path = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\image\\image.jpg"
         # enter_image_path = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\image\\0000.jpg"
-        template_image_folder = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\alcohol"
-        # template_image_folder = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}".format(product_type_title[0])
+        # template_image_folder = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\alcohol"
+        template_image_folder = "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}".format(product_type_title[0])
         print(template_image_folder)
 
         # template image
         templ = [os.path.join(template_image_folder, b) for b in os.listdir(template_image_folder) if
                  os.path.isfile(os.path.join(template_image_folder, b))]
-        # templ = [template for template in templ if template == "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}\\{}.jpg".format(product_type_title[0], product_title[0])]
-        templ = [template for template in templ if
-                 template == "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}\\{}.jpg".format(
-                     "alcohol", "t2")]
-
+        templ = [template for template in templ if template == "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}\\{}.jpg".format(product_type_title[0], product_title[0])]
+        # templ = [template for template in templ if
+        #          template == "C:\\Users\\savch\\PycharmProjects\\template-matcher\\data\\template\\{}\\{}.jpg".format(
+        #              "alcohol", "elab")]
 
         # shelf count
         # shelf_count = 2
@@ -221,16 +222,27 @@ class Main:
         for t in templ:
 
             img_tpl = cv2.imread(t, cv2.IMREAD_GRAYSCALE)
-
+            m_count = 0
             for img in crop_image_list:
                 shelf = int(img[len(img) - 5:len(img) - 4])
                 img_gray = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
-                coord = self.find_templ(img_gray, img_tpl)
+                find_temp_res = self.find_templ(img_gray, img_tpl)
+
+                coord = find_temp_res[0]
+                image_to_draw = find_temp_res[1]
 
                 # Match count on the shelf
-                match_count = len(coord)
+                tmp = 0
+                for x in coord:
+                    if x is not None:
+                        tmp += len(x)
+                print("tmp: ", tmp)
+                m_count += tmp
 
-                img_res = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+                if image_to_draw is None:
+                    img_res = cv2.cvtColor(img_gray, cv2.COLOR_GRAY2BGR)
+                else:
+                    img_res = cv2.cvtColor(image_to_draw, cv2.COLOR_GRAY2BGR)
                 img_res = self.draw_frames(img_res, coord)
                 tn = os.path.splitext(os.path.basename(img))[0]
                 if len(coord) != 0:
@@ -240,20 +252,22 @@ class Main:
                 # for c in coord:
                 #     print(c)
 
-                if len(coord) != 0:
-                    res_list.append(("res_{}_{}.jpg".format(tn, match_count), shelf, match_count))
+                # TODO: coord count
+                match_count += m_count
                 if match_count != 0:
-                    break
-            print(match_count)
+                    res_list.append(("res_{}_{}.jpg".format(tn, match_count), shelf, match_count))
+                # if match_count != 0:
+                #     break
+        print(match_count)
 
-        # estimate(self.image_key, match_count, shelf)
+        estimate(self.image_key, match_count, shelf)
 
-                # print("- - - - - - - - - - - - - - -")
+        # print("- - - - - - - - - - - - - - -")
 
 
 if __name__ == "__main__":
     # print("OpenCV ", cv2.__version__)
     # sys.exit(main())
-    main = Main(image_key="2018/07/28-17:37:06_sava_3", shelf_count=1)
+    main = Main(image_key="2018/09/08-15:52:18_sava_1", shelf_count=1)
     # main = Main(image_key=sys.argv[1], shelf_count=sys.argv[2])
     main.main()